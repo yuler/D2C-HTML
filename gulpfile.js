f52ae0564ae49e1a01dcb8ba95a778ef4290881b
joyful4yu <joyful4yu@gmail.com>
2016-08-08 11:05:58 +0800

    Add 简单的结构

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+var browserSync = require('browser-sync'),
+  gulp = require('gulp'),
+  stylus = require('gulp-stylus');
+
+/**
+ * 编译 stylus
+ */
+gulp.task('stylus', () => {
+  return gulp.src('./src/assets/stylus/base.styl')
+    .pipe(stylus({
+      compress: true
+    }))
+    .pipe(gulp.dest('./dist'))
+    .pipe(browserSync.stream())
+})
+
+/**
+ * html
+ */
+gulp.task('views', () => {
+  return gulp.src('./src/views/**/*.html')
+    .pipe(gulp.dest('./dist'))
+    .pipe(browserSync.stream())
+})
+
+/**
+ * 启动 browserSync 静态服务器
+ */
+gulp.task('serve', ['stylus', 'views'], function() {
+  browserSync({
+    notify: false,
+    port: 9000,
+    server: './dist'
+  })
+
+  gulp.watch('./src/assets/**/*.styl', ['stylus'])
+  gulp.watch('./src/views/**/*.html', ['views'])
+})
+
+gulp.task('default', ['serve'])
